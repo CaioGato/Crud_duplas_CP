a2e5f2347b585e7a2dc003c57df3ef08d6a7a6bf
Pavinis <160753747+Pavinis@users.noreply.github.com>
2024-09-30 16:01:49 -0300

    Update index.php

--- Bloco_notas_CP/index.php
+++ Bloco_notas_CP/index.php
@@ -1,4 +1,8 @@
 <?php
+// Modificação da atividade crud em duplas Johann Paulo.
+
+
+
 // Deve ser possível ver o quadro de horários com os dados da aula e tudo mais.
 
 // Além disso, deve ser possível ir até uma página para inserir um professor ou aula.
@@ -46,30 +50,24 @@ if ($result->num_rows > 0) {
 
 // Deletar após apertar o botão "Deletar Dados"
 if (isset($_POST["delete"])) {
-    if (isset($_POST['id_aula']) && !empty($_POST['id_aula'])) {
-        $id_del = $_POST['id_aula']; // Obtém o ID da aula a ser deletada
+    if (isset($_POST['id_nota']) && !empty($_POST['id_nota'])) {
+        $id_del = $_POST['id_nota']; // Obtém o ID da aula a ser deletada
         
         // Deletar da tabela `diario` e da tabela `aulas`
-        $sql_delete_diario = "DELETE FROM diario WHERE fk_aula = ?";
-        $sql_delete_aula = "DELETE FROM aulas WHERE id_aula = ?";
-        
-        // Preparar e executar a exclusão da tabela `diario`
-        $stmt_del_diario = $conn->prepare($sql_delete_diario);
-        $stmt_del_diario->bind_param('i', $id_del);
-        $stmt_del_diario->execute();
+        $sql_delete_nota = "DELETE FROM notas WHERE id_nota = ?";
         
         // Preparar e executar a exclusão da tabela `aulas`
-        $stmt_del_aula = $conn->prepare($sql_delete_aula);
-        $stmt_del_aula->bind_param('i', $id_del);
-        $stmt_del_aula->execute();
+        $stmt_del_nota = $conn->prepare($sql_delete_nota);
+        $stmt_del_nota->bind_param('i', $id_del);
+        $stmt_del_nota->execute();
 
-        if ($stmt_del_diario->affected_rows > 0 && $stmt_del_aula->affected_rows > 0) {
+        if ($stmt_del_nota->affected_rows > 0) {
             echo "Registro deletado com sucesso!";
         } else {
             echo "Erro ao deletar o registro.";
         }
     } else {
-        echo "ID da aula não encontrado.";
+        echo "ID da nota não encontrado.";
     }
     header ("Location: index.php");
     exit();
@@ -82,14 +80,14 @@ $conn -> close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud em dupla</title>
+    <title>Crud C. P.</title>
 </head>
 <body>
     
-    <h3>Quadro de Horário</h3>
+    <h3>Quadro de notas</h3>
     <h4>Ações:</h4>
-    <a href="add_professor.php"><button>Professores</button></a>
-    <a href="add_aula.php"><button>Criar Aulas</button></a>
-    <a href="alterar_dados.php"><button>Alterar Aulas</button></a>
+    <a href="create_usuario.php"><button>Página dos Usuários</button></a>
+    <a href="create_nota.php"><button>Criar Notas</button></a>
+    <a href="update_nota.php"><button>Alterar Notas</button></a>
 </body>
 </html>
