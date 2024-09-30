8db7c8d16c39ce3c93deb9087f663e9f7c2faa36
CaioBorba <160753615+CaioGato@users.noreply.github.com>
2024-09-30 12:05:54 -0300

    Update index.php

--- Bloco_notas_CP/index.php
+++ Bloco_notas_CP/index.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "bloco";
+$dbname = "bloco_notas_cp";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
